e3d7c1594b10ae7ca602a95caf5bf217c1f600b3
rahman <r.rezaee@webfume.com>
2021-03-06 11:42:57 +0430

    fix error

--- lib/modules/Resturant/Screen/formResturant.dart
+++ lib/modules/Resturant/Screen/formResturant.dart
@@ -31,7 +31,7 @@ class _ResturantFormState extends State<ResturantForm> {
             ),
             Row(children: [
               Container(
-                height: 150,
+                height: 130,
                 child: ClipRRect(
                   child: Image.network('https://i.pravatar.cc/300'),
                   borderRadius: BorderRadius.circular(10.0),
@@ -160,7 +160,7 @@ class _ResturantFormState extends State<ResturantForm> {
   Widget _dataBody() {
     return DataTable(
       dataTextStyle: TextStyle(
-        
+        color: Colors.black
       ),
       columns: [
       DataColumn(
