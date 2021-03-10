b30d232230281435143295fddc83cd055a5c4a31
Rahman Rezaee <rahmanrezaie60@gmail.com>
2021-03-10 19:38:49 +0430

    fix notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:admin/modules/Authentication/providers/auth_provider.dart';
 import 'package:admin/modules/Resturant/statement/resturant_provider.dart';
 import 'package:admin/modules/categories/provider/categories_provider.dart';
 import 'package:admin/modules/contactUs/providers/contact_provider.dart';
 import 'package:admin/modules/customers/provider/customers_provider.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:admin/modules/drawer/drawer.dart';
 import 'package:admin/modules/Authentication/screen/login_page.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import './themes/style.dart';
 import './routes.dart';
@@ -42,13 +45,142 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  initState() {
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  String fcmContactName;
+  String fcmContactId;
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
+
+  Future onSelectNotification(String payload) async {
+    Navigator.pushNamed(
+      context,
+      payload,
+      arguments: {
+        "contactId": fcmContactId,
+        "name": fcmContactName,
+      },
+    );
+  }
+
+  void notifyInitialize() async {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+    );
+    final MacOSInitializationSettings initializationSettingsMacOS =
+        MacOSInitializationSettings(
+            requestAlertPermission: false,
+            requestBadgePermission: false,
+            requestSoundPermission: false);
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIOS,
+            macOS: initializationSettingsMacOS);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
+  void showNotification(
+      {message, String routeName, BuildContext context, String userId}) async {
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+      Platform.isAndroid
+          ? 'com.dfa.flutterchatdemo'
+          : 'com.duytq.flutterchatdemo',
+      'Flutter chat demo',
+      'your channel description',
+      playSound: true,
+      enableVibration: true,
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      message['title'].toString(),
+      message['body'].toString(),
+      platformChannelSpecifics,
+      payload: routeName,
+      // payload: routeName + "::" + userId + "//" + message['title'],
+    );
+    print("Mahdi: routeName: $routeName");
+  }
+
+  void _navigateToItemDetail(Map<String, dynamic> message) {
+    fcmContactName = message['notification']['title'];
+    fcmContactId = message['data']['contactId'];
+
+    Navigator.pushNamed(
+      context,
+      message['data']['screen'],
+      arguments: {
+        "contactId": fcmContactId,
+        "name": fcmContactName,
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
     getPrefs();
     initUniLinks();
-    super.initState();
+    Future.delayed(Duration.zero, () {
+      notifyInitialize();
+      _firebaseMessaging.requestNotificationPermissions();
+      _firebaseMessaging.configure(
+        onMessage: (Map<String, dynamic> message) async {
+          print("onMessage:: $message");
+          print("onMessage:: ${message['data']['screen']}");
+
+          fcmContactName = message['notification']['title'];
+          fcmContactId = message['data']['contactId'];
+
+          Platform.isAndroid
+              ? showNotification(
+                  message: message['notification'],
+                  routeName: message['data']['screen'],
+                  userId: message['data']['contactId'],
+                  context: context,
+                )
+              : showNotification(
+                  message: message['aps']['alert'],
+                  routeName: message['data']['screen'],
+                  userId: message['data']['contactId'],
+                  context: context,
+                );
+
+          // _navigateToItemDetail(message);
+        },
+        onLaunch: (Map<String, dynamic> message) async {
+          print("onLaunch: $message");
+          print("onLaunch:: ${message['data']['screen']}");
+          _navigateToItemDetail(message);
+        },
+        onResume: (Map<String, dynamic> message) async {
+          print("onResume: $message");
+          print("onResume:: ${message['data']['screen']}");
+          _navigateToItemDetail(message);
+        },
+      );
+    });
   }
 
   StreamSubscription _sub;
+
+  
   Future<Null> initUniLinks() async {
     // Uri parsing may fail, so we use a try/catch FormatException.
     try {
--- lib/modules/categories/catetories_page.dart
+++ lib/modules/categories/catetories_page.dart
@@ -275,17 +275,17 @@ class _CatetoriesListPageState extends State<CatetoriesListPage> {
                                                         Navigator.of(context)
                                                             .pop();
                                                       } else {
-                                                        ScaffoldMessenger.of(
-                                                                context)
-                                                            .showSnackBar(
-                                                                SnackBar(
-                                                          content: const Text(
-                                                              'Something went wrong!'),
-                                                          duration:
-                                                              const Duration(
-                                                            seconds: 3,
-                                                          ),
-                                                        ));
+                                                        // ScaffoldMessenger.of(
+                                                        //         context)
+                                                        //     .showSnackBar(
+                                                        //         SnackBar(
+                                                        //   content: const Text(
+                                                        //       'Something went wrong!'),
+                                                        //   duration:
+                                                        //       const Duration(
+                                                        //     seconds: 3,
+                                                        //   ),
+                                                        // ));
                                                       }
                                                     });
                                                   }
@@ -431,12 +431,12 @@ class _CatetoriesListPageState extends State<CatetoriesListPage> {
                                         .then((re) {
                                       Navigator.of(context).pop();
                                       if (re['status'] == true) {
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(
-                                          SnackBar(
-                                              content: Text(
-                                                  "The Category Edited Successfully")),
-                                        );
+                                        // ScaffoldMessenger.of(context)
+                                        //     .showSnackBar(
+                                        //   SnackBar(
+                                        //       content: Text(
+                                        //           "The Category Edited Successfully")),
+                                        // );
                                       } else {}
                                     });
                                   } else {}
@@ -467,17 +467,17 @@ class _CatetoriesListPageState extends State<CatetoriesListPage> {
                                           .deleteCategoy(category.id)
                                           .then((res) {
                                         if (res['status']) {
-                                          ScaffoldMessenger.of(context)
-                                              .showSnackBar(SnackBar(
-                                            content: Text(
-                                                "The Category Deleted Successfully"),
-                                          ));
+                                          // ScaffoldMessenger.of(context)
+                                          //     .showSnackBar(SnackBar(
+                                          //   content: Text(
+                                          //       "The Category Deleted Successfully"),
+                                          // ));
                                           Navigator.of(context).pop();
                                         } else {
-                                          ScaffoldMessenger.of(context)
-                                              .showSnackBar(SnackBar(
-                                            content: Text(res['message']),
-                                          ));
+                                          // ScaffoldMessenger.of(context)
+                                          //     .showSnackBar(SnackBar(
+                                          //   content: Text(res['message']),
+                                          // ));
                                           Navigator.of(context).pop();
                                         }
                                       });
